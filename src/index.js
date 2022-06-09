dcaa8dc8d6e71a41dc269d4847a348691802e66b
cellle <91531988+cellle@users.noreply.github.com>
2022-06-09 13:07:15 +0200

    add browser-router

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Inbox from "./pages/Inbox";
+import Archive from "./pages/Archive";
+import Settings from "./pages/Settings";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Inbox />} />
+        <Route path="/archive" element={<Archive />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
