38e51fb514061fee359ed0a143f774c3a947f710
cellle <91531988+cellle@users.noreply.github.com>
2022-06-13 13:09:01 +0200

    add fetches

---
+++ src/hooks/useArticles.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+const useArticles = (category, numberOfArticles) => {
+  const [articles, setArticles] = useState(null);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=PF8A2Jr4UOKYnv9hbdHsEAvBgeBEjLDw`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("Could not fetch the data for that resource");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setArticles(data.results.slice(0, numberOfArticles));
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((error) => {
+        setIsPending(false);
+        setError(error.message);
+      });
+  }, []);
+
+  return { articles, error, isPending };
+};
+
+export default useArticles;
