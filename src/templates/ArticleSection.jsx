20e3e2da78a27107de951f8ff2b390c8a22f1e24
cellle <91531988+celinerh@users.noreply.github.com>
2022-06-15 15:07:35 +0200

    add style

--- src/templates/ArticleSection.jsx
+++ src/templates/ArticleSection.jsx
@@ -1,25 +1,71 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
 import Article from "../components/Article";
 import useArticles from "../hooks/useArticles";
+import { IoIosArrowDown } from "react-icons/io";
+import { BiCategoryAlt } from "react-icons/bi";
 
 const ArticleSection = ({ category }) => {
   const { articles, error, isPending } = useArticles(category, 5);
 
-  articles && console.log("articles", articles);
+  const styles = {
+    component: css``,
+    categoryHeader: css`
+      padding: 10px;
+      display: grid;
+      grid-template-columns: auto 1fr auto;
+      align-items: center;
+
+      border-top: 1px solid red;
+      border-bottom: 1px solid red;
+
+      & svg {
+        display: block;
+        font-size: 1.4rem;
+      }
+
+      & h2 {
+        margin-left: 10px;
+        text-transform: uppercase;
+        font-size: 1.1rem;
+      }
+    `,
+    iconContainer: css`
+      background-color: red;
+      padding: 5px;
+      border-radius: 50%50%;
+    `,
+    articles: css`
+      & div:not(:last-child) {
+        border-bottom: 1px solid orange;
+      }
+    `,
+  };
 
   return (
-    <div>
-      <h2>{category}</h2>
-      {articles &&
-        articles.map((article, index) => {
-          return (
-            <Article
-              imageSource={article.multimedia[2].url}
-              headline={article.title}
-              abstract={article.abstract}
-              key={index}
-            />
-          );
-        })}
+    <div css={styles.component}>
+      <div css={styles.categoryHeader}>
+        <div css={styles.iconContainer}>
+          <BiCategoryAlt />
+        </div>
+        <h2>{category}</h2>
+        <IoIosArrowDown />
+      </div>
+
+      <div css={styles.articles}>
+        {articles &&
+          articles.map((article, index) => {
+            return (
+              <Article
+                imageSource={article.multimedia[0].url}
+                headline={article.title}
+                abstract={article.abstract}
+                key={index}
+              />
+            );
+          })}
+      </div>
     </div>
   );
 };
