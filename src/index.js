90643473f4a637855fbc23b4b10f43eef302c85f
cellle <91531988+celinerh@users.noreply.github.com>
2022-06-15 15:06:58 +0200

    remove strict mode

--- src/index.js
+++ src/index.js
@@ -9,14 +9,12 @@ import Settings from "./pages/Settings";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Inbox />} />
-        <Route path="/archive" element={<Archive />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<Inbox />} />
+      <Route path="/archive" element={<Archive />} />
+      <Route path="/settings" element={<Settings />} />
+    </Routes>
+  </BrowserRouter>
 );
