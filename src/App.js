dbfd9146ff7c680bbf156e023578050faf2f0be0
cellle <91531988+cellle@users.noreply.github.com>
2022-06-17 10:40:10 +0200

    refactor browserrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import Navigation from "./components/Navigation";
+import { SettingsContext } from "./context/Contexts";
+import { Outlet } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [categorySettings, setCategorySettings] = useState({});
+
+  return (
+    <div className="App">
+      <SettingsContext.Provider
+        value={{ categorySettings, setCategorySettings }}
+      >
+        <Navigation />
+        <Outlet />
+      </SettingsContext.Provider>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navigation from "./components/Navigation";
 import Inbox from "./pages/Inbox";
 import Archive from "./pages/Archive";
 import Settings from "./pages/Settings";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Navigation />
     <Routes>
-      <Route path="/" element={<Inbox />} />
-      <Route path="/archive" element={<Archive />} />
-      <Route path="/settings" element={<Settings />} />
+      <Route element={<App />}>
+        <Route path="/" element={<Inbox />} />
+        <Route path="/archive" element={<Archive />} />
+        <Route path="/settings" element={<Settings />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 );
