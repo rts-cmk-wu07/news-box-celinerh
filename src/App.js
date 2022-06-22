b444978b32f8b0ad6c760a70675b0fa3273c7263
cellle <91531988+celinerh@users.noreply.github.com>
2022-06-22 10:49:00 +0200

    create section and archive context

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import { css } from "@emotion/react";
 
 import "./App.css";
 import Navigation from "./components/Navigation";
-import { DarkModeContext, SettingsContext } from "./context/Contexts";
+import {
+  ArchiveContext,
+  DarkModeContext,
+  SectionsContext,
+  SettingsContext,
+} from "./context/Contexts";
 import { Outlet } from "react-router-dom";
 import { useState } from "react";
 
 function App() {
+  const [sections, setSections] = useState([]);
+
   const [settings, setSettings] = useState({
     sections: null,
   });
+
+  const [archive, setArchive] = useState({
+    articles: [],
+  });
+
   const [darkMode, setDarkMode] = useState(false);
 
   const styles = {
@@ -24,12 +36,16 @@ function App() {
 
   return (
     <div css={styles.app} className={darkMode ? "dark-mode" : ""}>
-      <DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
-        <SettingsContext.Provider value={{ settings, setSettings }}>
-          <Navigation />
-          <Outlet />
-        </SettingsContext.Provider>
-      </DarkModeContext.Provider>
+      <SectionsContext.Provider value={{ sections, setSections }}>
+        <DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
+          <SettingsContext.Provider value={{ settings, setSettings }}>
+            <ArchiveContext.Provider value={{ archive, setArchive }}>
+              <Navigation />
+              <Outlet />
+            </ArchiveContext.Provider>
+          </SettingsContext.Provider>
+        </DarkModeContext.Provider>
+      </SectionsContext.Provider>
     </div>
   );
 }
--- src/context/Contexts.jsx
+++ src/context/Contexts.jsx
@@ -1,7 +1,13 @@
 import { createContext } from "react";
 
+export const SectionsContext = createContext([]);
+
 export const SettingsContext = createContext({
   sections: null,
+});
+
+export const ArchiveContext = createContext({
+  articles: [],
 });
 
 export const DarkModeContext = createContext(false);
